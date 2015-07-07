e99efc9642714f299eac3ee937099a3bee4c3873
pedrobenevides86 <pedrobenevidesb@gmail.com>
2015-07-07 20:05:05 -0300

    Configurando o Front-End da camada web.

---
+++ SistemaDeChamados.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace SistemaDeChamados.Web.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap*"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryValidate").Include("~/Scripts/jquery.validate*"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap*"));
+        }
+    }
+}
