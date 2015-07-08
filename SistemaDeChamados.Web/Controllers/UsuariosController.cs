ec97b8752302a166300e645af004b5f47353a3d2
pedrobenevides86 <pedrobenevidesb@gmail.com>
2015-07-07 22:16:38 -0300

    Controller de Usuarios, e instalacao do EF na camada Web

---
+++ SistemaDeChamados.Web/Controllers/UsuariosController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using SistemaDeChamados.Application.Interface;
+
+namespace SistemaDeChamados.Web.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private readonly IUsuarioAppService usuarioAppService;
+
+        public UsuariosController(IUsuarioAppService usuarioAppService)
+        {
+            this.usuarioAppService = usuarioAppService;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(usuarioAppService.Retrieve());
+        }
+    }
+}
