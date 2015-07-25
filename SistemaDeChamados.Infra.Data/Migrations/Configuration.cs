19f6a70156bdd759a7c7f83e1a8f9ac29de3bdf9
pedrobenevides86 <pedrobenevidesb@gmail.com>
2015-07-25 09:03:20 -0300

    Ajuste no Seed

--- SistemaDeChamados.Infra.Data/Migrations/Configuration.cs
+++ SistemaDeChamados.Infra.Data/Migrations/Configuration.cs
@@ -1,23 +1,25 @@
-using System;
-using System.Linq;
+using System.Data.Entity.Migrations;
+using System.Data.Entity.Validation;
+using System.Text;
 using SistemaDeChamados.Domain.Entities;
-using SistemaDeChamados.Domain.Enums;
+using SistemaDeChamados.Domain.Services;
+using SistemaDeChamados.Infra.Data.Contexto;
 
 namespace SistemaDeChamados.Infra.Data.Migrations
 {
-    using System.Data.Entity.Migrations;
-
-    internal sealed class Configuration : DbMigrationsConfiguration<Contexto.SistemaContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<SistemaContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
         }
 
-        protected override void Seed(Contexto.SistemaContext context)
+        protected override void Seed(SistemaContext context)
         {
-            context.Usuarios.Add(new Usuario("teste@mail.com", "Pedro Benevides"));
-            var usuario = context.Usuarios.FirstOrDefault();
+            var usuario = new Usuario("teste@mail.com", "Pedro Benevides");
+            usuario.DefinirPassword("123456", new CriptografadorDeSenhaMD5());
+            context.Usuarios.Add(usuario);
+            //var usuario = context.Usuarios.FirstOrDefault();
 
             //context.Chamados.Add(new Chamado
             //{
@@ -29,7 +31,26 @@ namespace SistemaDeChamados.Infra.Data.Migrations
                 
             //});
 
-            context.SaveChanges();
+            try
+            {
+                context.SaveChanges();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                var sb = new StringBuilder();
+
+                foreach (var failure in ex.EntityValidationErrors)
+                {
+                    sb.AppendFormat("{0} failed validation\n", failure.Entry.Entity.GetType());
+                    foreach (var error in failure.ValidationErrors)
+                    {
+                        sb.AppendFormat("- {0} : {1}", error.PropertyName, error.ErrorMessage);
+                        sb.AppendLine();
+                    }
+                }
+
+                throw new DbEntityValidationException("Entity Validation Failed - errors follow:\n" + sb.ToString(), ex); 
+            }
         }
     }
 }
