b371253264d3e183b1cb53558704a48bc31b2fbd
pedrobenevides86 <pedrobenevidesb@gmail.com>
2015-08-10 12:23:11 -0300

    Adicionando HttpModule'

---
+++ SistemaDeChamados.Web/Modules/RemoveHeadersModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web;
+
+namespace SistemaDeChamados.Web.Modules
+{
+    public class RemoveHeadersModule : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            context.PreSendRequestHeaders += Application_PreSendRequestHeaders;
+        }
+
+        public void Dispose()
+        { }
+
+        private static void Application_PreSendRequestHeaders(object sender, EventArgs e)
+        {
+            var context = (HttpApplication)sender;
+
+            if(context == null || context.Context == null) return;
+
+            context.Context.Response.Headers.Remove("Server");
+            context.Context.Response.Headers.Remove("X-AspNet-Version");
+            context.Context.Response.Headers.Remove("X-AspNetMvc-Version");
+        }
+    }
+}
