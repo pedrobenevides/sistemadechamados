141ecc8dc3c59d3c665fb5c217f4fed37bbb5ccd
Pedro Benevides <pedrobenevidesb@gmail.com>
2015-09-03 17:56:38 -0300

    Html Helper para a Sidebar

---
+++ SistemaDeChamados.Web/Helpers/Listas.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Web.Mvc;
+
+namespace SistemaDeChamados.Web.Helpers
+{
+    public static class Listas
+    {
+        public static MvcHtmlString SideBarItens(this HtmlHelper helper, IDictionary<string, string> model)
+        {
+            var stringBuilder = new StringBuilder();
+
+            foreach (var item in model)
+                stringBuilder.Append(string.Format("<li><a href=\"Index/{0}\">{1}</a></li>", item.Key, item.Value));
+
+            return MvcHtmlString.Create(stringBuilder.ToString());
+        }
+    }
+}
