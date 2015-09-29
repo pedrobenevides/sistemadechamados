919d9c633340f4f2b797feb1a5f5f29ed41d9d8d
pedrobenevides86 <pedrobenevidesb@gmail.com>
2015-09-28 22:52:51 -0300

    Loading no botao de salvar msg

---
+++ SistemaDeChamados.Web/Scripts/Layout/master.js
@@ -0,0 +1,16 @@
+﻿$(function() {
+
+    (function montarBreadCrumb() {
+        var urlPathArray = window.location.pathname.split("/");
+        var controller = urlPathArray[1];
+        var action = urlPathArray[2];
+
+        if (action === undefined)
+            action = "Index";
+
+        if (controller !== "Home" && controller !== "") {
+            $('ol').append('<li><a href="/' + controller + '">' + controller + '</a></li>');
+            $('ol').append('<li>' + action + '</li>');
+        }
+    })();
+});
